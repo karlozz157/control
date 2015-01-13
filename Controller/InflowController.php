9eb1c1d3e691094d41b7bd582cde35bfd7a2c685
karlozz157 <karlozz157@gmal.com>
2015-01-12 21:01:07 -0600

    Added controller structure

---
+++ Controller/InflowController.php
@@ -0,0 +1,24 @@
+<?php
+
+class InflowController extends Controller
+{
+    public function indexAction()
+    {
+
+    }
+
+    public function newAction()
+    {
+        
+    }
+
+    public function editAction()
+    {
+
+    }
+
+    public function deleteAction()
+    {
+
+    }
+}
---
+++ Controller/OutflowController.php
@@ -0,0 +1,24 @@
+<?php
+
+class OutflowController extends Controller
+{
+    public function indexAction()
+    {
+
+    }
+
+    public function newAction()
+    {
+        
+    }
+
+    public function editAction()
+    {
+
+    }
+
+    public function deleteAction()
+    {
+
+    }
+}
